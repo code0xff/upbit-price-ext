53a6adb0898a7df35360e623a6f4bad1ef4cc6e2
Ian J.Um <31336310+code0xff@users.noreply.github.com>
2022-03-05 01:10:27 +0900

    feat: add auto refresh

--- popup.js
+++ popup.js
@@ -1,5 +1,8 @@
-var port = chrome.runtime.connect({name: 'market'});
+var port = chrome.runtime.connect({name: 'market'})
 var ticker = 'ETH'
+var autoRefresh = false
+var autoRefreshId
+var beforePrice = 0
 
 document.getElementById('get-price').addEventListener('click', function() {
   port.postMessage({market: 'KRW-' + ticker})
@@ -7,8 +10,48 @@ document.getElementById('get-price').addEventListener('click', function() {
 
 document.getElementById('select-ticker').addEventListener('change', function() {
   ticker = this.value
+  document.getElementById('price-value').innerHTML = 0
+})
+
+document.getElementById('auto-refresh').addEventListener('click', function() {
+  if (autoRefresh) {
+    document.getElementById('auto-refresh').className = 'default-button'
+    document.getElementById('get-price').className = 'default-button'
+    document.getElementById('get-price').disabled = false
+    document.getElementById('price-value').className = 'default'
+
+    document.getElementById('select-ticker').disabled = false
+    document.getElementById('select-ticker').className = 'default-select'
+
+    clearInterval(autoRefreshId)
+  } else {
+    document.getElementById('auto-refresh').className = 'success-button'
+    document.getElementById('get-price').className = 'disabled-button'
+    document.getElementById('get-price').disabled = true
+
+    document.getElementById('price-value').innerHTML = 0
+    document.getElementById('select-ticker').disabled = true
+    document.getElementById('select-ticker').className = 'disabled-select'
+
+    autoRefreshId = setInterval(function() {
+      port.postMessage({market: 'KRW-' + ticker})
+    }, 1000)
+  }
+  autoRefresh = !autoRefresh
 })
 
 port.onMessage.addListener(function(message) {
-  document.getElementById('price-value').innerHTML = parseInt(message.price).toLocaleString()
+  const currentPrice = parseInt(message.price)
+  if (autoRefresh) {
+    if (currentPrice === beforePrice) {
+      document.getElementById('price-value').className = 'default'
+    } else if (currentPrice > beforePrice) {
+      document.getElementById('price-value').className = 'danger'
+    } else {
+      document.getElementById('price-value').className = 'primary'
+    }
+    beforePrice = currentPrice
+  }
+
+  document.getElementById('price-value').innerHTML = currentPrice.toLocaleString()
 })
